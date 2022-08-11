12d01482a61a07c4496421e0bcac1efae1228424
Iván Nzohad Casas <100694470+IVI-UNI@users.noreply.github.com>
2022-08-11 06:10:57 +0200

    C code TCP por socket

--- cliente.c
+++ cliente.c
@@ -1,27 +1,65 @@
-//#include <netinet/in.h> //structure for storing address information
+#include <netinet/in.h> //structure for storing address information
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h> //for socket APIs
 #include <sys/types.h>
-#include <servidor.h>
+#include <errno.h>
+#include "servidor.h"
 
-int main(int argvc, const* char argv[]{
+
+int main(int argvc, char const* argv[])
+{
+
+	uint32_t parametros[4]; //Parametros de la solicitud
+	uint32_t respuesta;
+	uint32_t contarPrimos = 1;
+  	uint32_t obtenerPrimos = 2;
+
+	//Asignación de valores al mensaje de solicitud
+	parametros[0] = htonl(atoi(argv[1]));	//NIP	
+	parametros[1] = htonl(atoi(argv[2]));	//Servicio
+	parametros[2] = htonl(atoi(argv[3]));	//Rango 0
+	parametros[3] = htonl(atoi(argv[4]));	//Rango 1
+
 	int sock = socket(AF_INET, SOCK_STREAM, 0);
-	sturct sockaddr_in sockDirecciones;
+	struct sockaddr_in sockDirecciones;
 
 	sockDirecciones.sin_family = AF_INET;
 	sockDirecciones.sin_addr.s_addr = INADDR_ANY;
-	sockDirecciones.sin_port = PUERTO;
+	sockDirecciones.sin_port = htons(PUERTO);
 
-	int estadoConexion = connect(sock, (struct sockDirecciones*)&servAddr, sizeof(servAddr));
+	int estadoConexion = connect(sock, (struct sockaddr*)&sockDirecciones, sizeof(sockDirecciones));
 
-	if (estadoConexion == -1){}
+	if (estadoConexion == -1){
 		printf("Error en la conexion \n");
+		printf("ERROR: %s\n", strerror(errno));
 	}else{
+		printf("Cliente conectado a servidor, escriba su mensaje: \n");
 		char mensaje[255];
-		recv(sock, mensaje, sizeof(mensaje), 0)
+		recv(sock, mensaje, sizeof(mensaje), 0);
 		printf("Mensaje: %s\n", mensaje);
-	}
 
+		int n = write(sock, parametros, 4*sizeof(uint32_t));
+		//int n = send(sock, parametros, sizeof(parametros), 0);
+
+		if (n<0){
+			printf("Error al escribir \n");
+		}else{
+			if (atoi(argv[2]) == contarPrimos){
+				int nPrimos;
+				read(sock, &nPrimos, sizeof(nPrimos));
+				printf("Entre el intervalo %d y - %d se han encontrado %d numeros primos", atoi(argv[3]), atoi(argv[4]), nPrimos);
+			}else if(atoi(argv[2]) == obtenerPrimos) {
+				printf("a obtenerPrimos");
+				uint32_t vectorPrimos[100];
+				read(sock, vectorPrimos, sizeof(vectorPrimos));
+				printf("a obtenerPrimos");
+				for(int i=0; i <5; i++)
+					printf("\t %u", ntohl(vectorPrimos[i]));
+			}
+
+		}
+	}
+	close(sock);
 	return 0;
-})
+}
--- servidor.c
+++ servidor.c
@@ -1,5 +1,9 @@
 #include "servidor.h"
-
+#include <netinet/in.h> //structure for storing address information
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h> //for socket APIs
+#include <sys/types.h>
 
 int cuenta_primos(int min, int max)
 {
@@ -65,12 +69,61 @@ int esprimo(int n)
 /* Escribe una lista de números primos entre 1 y 1000000 */
 int main()
 {
-	int i, cuantos;
-	int primos[10000];
-	cuantos = cuenta_primos(MINIMO, MAXIMO);
-	printf("\n El número de primos es: %d \n", cuantos);
-	cuantos = encuentra_primos(MINIMO, MAXIMO, primos);
-	for (i = 0; i < cuantos; i++)
-		printf("\n %ld es primo \n", primos[i]);
-	exit(0);
+  uint32_t parametrosNet[4];
+  uint32_t NIP;
+  uint32_t servicio;
+  uint32_t min;
+  uint32_t max;
+  uint32_t contarPrimos = 1;
+  uint32_t obtenerPrimos = 2;
+
+  printf("Tamaño un uint32_t : %lu \n", 4*sizeof(uint32_t));
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+  struct sockaddr_in dirServ;
+  dirServ.sin_family = AF_INET;
+  dirServ.sin_port = htons(PUERTO);
+  dirServ.sin_addr.s_addr = INADDR_ANY;
+
+  char mensaje[255] = " Aquí está el mensaje que esperabas \n SURPRISE MADAFAKA";
+
+  bind(sock, (struct sockaddr*)&dirServ, sizeof(dirServ));
+
+  listen(sock, 1);
+
+  int clienteSocket = accept(sock, NULL, NULL);
+
+  send(clienteSocket, mensaje, sizeof(mensaje), 0);
+
+  bzero(parametrosNet, 4*sizeof(uint32_t));
+  int n = read(clienteSocket, parametrosNet, 4*sizeof(uint32_t));
+  //int n = recv(clienteSocket, parametrosNet, sizeof(parametros), 0);
+  NIP = htonl(parametrosNet[0]);
+  servicio = htonl(parametrosNet[1]);
+  min= htonl(parametrosNet[2]);
+  max= htonl(parametrosNet[3]);
+
+  if (n < 0){
+    printf("Error leyendo los paramentros del cliente");
+  }else{
+    printf("Cliente NIP: %u \n", NIP);
+    printf("Cliente servicio: %u \n", servicio);
+    printf("Cliente min: %u \n", min);
+    printf("Cliente max: %u \n", max);
+
+    if (servicio == contarPrimos){
+      int nPrimos = cuenta_primos(min, max);
+      printf("Entre el intervalo %u y - %u se han encontrado %d numeros primos\n", min, max, nPrimos);
+      write(clienteSocket, &nPrimos, sizeof(nPrimos));
+    }else if( servicio == obtenerPrimos){
+      uint32_t vectorPrimos[100];
+      int nPrimos = net_encuentra_primos(min, max, vectorPrimos);
+      //printf("Entre el intervalo %u - %u se han encontrado los siguientes numeros primos: %d \n", min, max. nPrimos);
+      for (int i = nPrimos; i > 0; i--)
+        printf("\t %u", ntohl(vectorPrimos[i]));
+      write(clienteSocket, vectorPrimos, sizeof(vectorPrimos));
+    }
+    close(clienteSocket);
+    close(sock);
+    return 0;
+  }
 }
