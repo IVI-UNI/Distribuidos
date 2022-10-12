85bc32817f684244ff23f108e5b3fd6b3926e108
Iván Nzohad Casas <100694470+IVI-UNI@users.noreply.github.com>
2022-10-12 12:13:59 +0200

    Servidor C parte A

--- servidor.c
+++ servidor.c
@@ -5,14 +5,8 @@
 #include <sys/socket.h> //for socket APIs
 #include <sys/types.h>
 
-int cuenta_primos(int min, int max)
-{
+int cuenta_primos(int min, int max){
   int i, contador;
-// long cuenta_primos(long min, long max)		//
-// {							//
-//   long i, contador;					//
-
-
   contador = 0;
   
   for (i = min; i <= max; i++)
@@ -22,12 +16,8 @@ int cuenta_primos(int min, int max)
 }
 
 
-int encuentra_primos(int min, int max, int vector[])
-{
+int encuentra_primos(int min, int max, int vector[]){
   int i, contador;
-// long encuentra_primos(long min, long max, long vector[])	//
-// {								//
-//   long i, contador;						//
 
   contador = 0;
 
@@ -38,27 +28,28 @@ int encuentra_primos(int min, int max, int vector[])
 }
 
 
-int net_encuentra_primos(int min, int max, uint32_t vector[])
-{
+int net_encuentra_primos(int min, int max, uint32_t *vector, int *tamanoVector){
   int i, contador;
-// long net_encuentra_primos(long min, long max, uint32_t vector[])	//
-// {									//
-//   long i, contador;							//
+  uint32_t *vectorAux;
 
   contador = 0;
 
   for (i = min; i <= max; i++)
-    if (esprimo(i)) vector[contador++] = htonl(i);
-
+    if (esprimo(i)){
+      vector[contador++] = htonl(i);
+      if (contador == *tamanoVector){
+        printf("Se ha reservado espacio en memoria de forma dinámica \n");
+        *tamanoVector += 10;
+        printf("tamano vector = %d", *tamanoVector);
+        vectorAux = (uint32_t*)realloc(vector, *tamanoVector*sizeof(uint32_t));
+        vector = vectorAux;
+      }
+    }
   return (contador);
 }
 
-int esprimo(int n)
-{
+int esprimo(int n){
   int i;
-// int esprimo(long n)			//
-// {					//
-//   long i;				//
 
   for (i = 2; i*i <= n; i++)
     if ((n % i) == 0) return (0);
@@ -69,6 +60,7 @@ int esprimo(int n)
 /* Escribe una lista de números primos entre 1 y 1000000 */
 int main()
 {
+  //variables 
   uint32_t parametrosNet[4];
   uint32_t NIP;
   uint32_t servicio;
@@ -76,54 +68,77 @@ int main()
   uint32_t max;
   uint32_t contarPrimos = 1;
   uint32_t obtenerPrimos = 2;
-
-  printf("Tamaño un uint32_t : %lu \n", 4*sizeof(uint32_t));
+  
+  //creación del socket
   int sock = socket(AF_INET, SOCK_STREAM, 0);
   struct sockaddr_in dirServ;
   dirServ.sin_family = AF_INET;
   dirServ.sin_port = htons(PUERTO);
   dirServ.sin_addr.s_addr = INADDR_ANY;
 
-  char mensaje[255] = " Aquí está el mensaje que esperabas \n SURPRISE MADAFAKA";
-
   bind(sock, (struct sockaddr*)&dirServ, sizeof(dirServ));
 
-  listen(sock, 1);
+  listen(sock, 10);
+  while(1){
 
-  int clienteSocket = accept(sock, NULL, NULL);
+    int clienteSocket = accept(sock, NULL, NULL);
+    if(fork() == 0){   
+      //Lectura de los parametros de la solicitud de servicio
+      bzero(parametrosNet, 4*sizeof(uint32_t));
+      int n = read(clienteSocket, parametrosNet, 4*sizeof(uint32_t));
+      NIP = parametrosNet[0];
+      servicio = htonl(parametrosNet[1]);
+      min= htonl(parametrosNet[2]);
+      max= htonl(parametrosNet[3]);
 
-  send(clienteSocket, mensaje, sizeof(mensaje), 0);
+      if (n < 0){
+        printf("Error leyendo los paramentros del cliente");
+      }else{
+        printf("Cliente NIP: %u \n", NIP);
+        printf("Servicio solicitado: %u \n", servicio);
+        printf("Cliente min: %u \n", min);
+        printf("Cliente max: %u \n", max);
+        printf("Cliente PID: %d \n", getpid());
 
-  bzero(parametrosNet, 4*sizeof(uint32_t));
-  int n = read(clienteSocket, parametrosNet, 4*sizeof(uint32_t));
-  //int n = recv(clienteSocket, parametrosNet, sizeof(parametros), 0);
-  NIP = htonl(parametrosNet[0]);
-  servicio = htonl(parametrosNet[1]);
-  min= htonl(parametrosNet[2]);
-  max= htonl(parametrosNet[3]);
+        if (servicio == contarPrimos){
+          int nPrimos = cuenta_primos(min, max);
+          printf("Entre el intervalo %u y - %u se han encontrado %d numeros primos\n", min, max, nPrimos);
+          write(clienteSocket, &nPrimos, sizeof(nPrimos));
+        
+        }else if( servicio == obtenerPrimos){
+          //Contar primos en el intervalo dado para saber el tamaño del vector
+          int nPrimos = cuenta_primos(min, max);
+          //Vector tipo int con los primos 
+          int vectorPrimosI[nPrimos]; 
+          //Vector de tipo uint_32t con los primos
+          uint32_t vectorPrimosU[nPrimos];
+          //Vector con los primos en formato red
+          uint32_t vectorPrimos[nPrimos];
 
-  if (n < 0){
-    printf("Error leyendo los paramentros del cliente");
-  }else{
-    printf("Cliente NIP: %u \n", NIP);
-    printf("Cliente servicio: %u \n", servicio);
-    printf("Cliente min: %u \n", min);
-    printf("Cliente max: %u \n", max);
+          encuentra_primos(min, max, vectorPrimosI);
 
-    if (servicio == contarPrimos){
-      int nPrimos = cuenta_primos(min, max);
-      printf("Entre el intervalo %u y - %u se han encontrado %d numeros primos\n", min, max, nPrimos);
-      write(clienteSocket, &nPrimos, sizeof(nPrimos));
-    }else if( servicio == obtenerPrimos){
-      uint32_t vectorPrimos[100];
-      int nPrimos = net_encuentra_primos(min, max, vectorPrimos);
-      //printf("Entre el intervalo %u - %u se han encontrado los siguientes numeros primos: %d \n", min, max. nPrimos);
-      for (int i = nPrimos; i > 0; i--)
-        printf("\t %u", ntohl(vectorPrimos[i]));
-      write(clienteSocket, vectorPrimos, sizeof(vectorPrimos));
+
+          for (int i = 0; i < nPrimos; i++){
+            vectorPrimosU[i] = vectorPrimosI[i];        //Convertir de unsigned a int
+            vectorPrimos[i] = ntohl(vectorPrimosU[i]);  //Guardar el vector de tipo unsigned con formato red
+          }
+
+          printf("se ha ejecutado la funcion de encontrar primos \n");
+          printf("Primos encontrados entre  %u  y  %u :  %d \n", min, max, nPrimos);
+          int tamanoFinal = ntohl(nPrimos);
+          write(clienteSocket, &tamanoFinal, sizeof(int));   //Enviar el tamaño del array
+          int bytes_escritos = write(clienteSocket, &vectorPrimos, nPrimos*sizeof(uint32_t));
+          for (int i = 0; i < nPrimos; i++){
+            printf("\t %d ", htonl(vectorPrimos[i]));
+          }
+        }
+        close(clienteSocket);
+        exit(0);
+      }
+    }else{
+      printf("Soy el proceso padre %d y voy a terminar", getpid());    
+      close(sock);
+      exit(0)
     }
-    close(clienteSocket);
-    close(sock);
-    return 0;
   }
 }
